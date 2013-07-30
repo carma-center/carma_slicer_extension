b921dccf116378fb5d0d9e068d44300e442ce799
sbengali <salma.bengali@carma.utah.edu>
2013-07-30 12:26:40 -0600

    Updated license and documentation

--- CMRToolkitLASegmentationGraphCuts/CMRToolkitLASegmentationGraphCuts.cxx
+++ CMRToolkitLASegmentationGraphCuts/CMRToolkitLASegmentationGraphCuts.cxx
@@ -1,4 +1,26 @@
-// This code performs graph cuts based on Vnet strategy, with intensity profiles as the cost functions
+/*
+  This code performs graph cuts based on Vnet strategy, with intensity profiles as the cost functions 
+*/
+
+/*This program uses the maxflow algorithm
+	described in
+
+		"An Experimental Comparison of Min-Cut/Max-Flow Algorithms for Energy Minimization in Vision."
+		Yuri Boykov and Vladimir Kolmogorov.
+		In IEEE Transactions on Pattern Analysis and Machine Intelligence (PAMI), 
+		September 2004
+
+	This algorithm was developed by Yuri Boykov and Vladimir Kolmogorov
+	at Siemens Corporate Research. To make it available for public use,
+	it was later reimplemented by Vladimir Kolmogorov based on open publications.
+
+	If you use this software for research purposes, you should cite
+	the aforementioned paper in any resulting publication.
+*/
+	
+/*
+	For description, license, example usage see README.TXT.
+*/
 
 #if defined(_MSC_VER)
 #pragma warning ( disable : 4786 )
